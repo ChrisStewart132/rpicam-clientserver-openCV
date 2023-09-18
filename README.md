22f6d67d7b80505fc4083e4146ff941775b2f5ad
Christopher Stewart <chris.stewart132@gmail.com>
2023-09-19 09:55:21 +1200

    testing server and refactoring, update README

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
 # rpicam-clientserver-openCV
-simple raspberry pi camera python script to send the camera frames to a specified server address
 
+# description
+raspberry pi camera python script to send the camera frames to a specified server address
 server python script uses openCV library to alter the recieved images and display them
+note: MJPEG format is used as openCV2 provides a decoding function, h264 would be more efficient in terms of data rate
 
-note: the current implementation recieves frames in the form of mjpeg frames and should be optimised (better understood)
+client(rpi) script continuously records sending the data to the given addr:port (tcp)
+server(probably windows) listens, connects (tcp), displays images as they come, keys: q=quit, b=backsub, c=corner, s=contours, t=thresholding
 
+# how to start
+raspberry pi: cntrl+alt+t or ssh pi@192.168.1.66 to open cmd line
+	# run cmd in the dir of the script
+	python picamera_client.py SERVER_ADDR SERVER_PORT
+	python picamera_client.py 192.168.1.68 8002
 
-workflow:
-	ssh pi@192.168.1.66
-	from windows powershell (shift right click in file exporer dir -> powershell here)
-		scp picamera_client.py pi@192.168.1.66 #(scp -options source destination)
+windows: shift right click in dir-> open windows power shell
+	# to send client script to your rpi
+	scp picamera_client.py pi@192.168.1.66 #(scp -options source destination)
 
-	in the ssh of pi run
-		python3 picamera_client.py
+	# to run the server
+	py picamera_server.py SERVER_PORT
+	py picamera_server.py 8002
+
+# docs
+https://picamera.readthedocs.io/en/release-1.13/api_camera.html
+https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
--- picamera_server.py
+++ picamera_server.py
@@ -3,15 +3,24 @@ import cv2
 import socket
 import subprocess
 import time
+import sys
+# Check the number of command-line arguments
+if len(sys.argv) < 2:
+    print("Usage: py" + argv[0] + ".py PORT_NUMBER")
+    sys.exit(1)  # Exit with an error code
 
+PORT_NUMBER = int(sys.argv[1])
 
-BACKGROUND_SUBTRACTOR = False #white for movement, black for background
-CORNER_DETECTION = False #finds corners and draws circles upon them
-THRESHOLDING = True #thresholds out dark colours to black, other to white 
-COUNTOURS =  False
+SOCKET_TIMEOUT = 1000# how long the socket waits to recieve data before closing
 
-WINDOW_NAME = "window"
-SCAN_LEN_EST = 50000# guessing len of SOS, if wrong frame broken/merged
+BACKGROUND_SUBTRACTOR = False # detects movement: white for movement, black for background
+CORNER_DETECTION = False # draws circles on detected corners
+THRESHOLDING = False# shows the foreground and ignores the background, thresholds out dark colours to black, others to white
+COUNTOURS =  False# shape analysis and object detection, draws detected shapes on a black background
+# toggle keys b,c,t,s
+
+WINDOW_NAME = "rpicam-clientserver-openCV-CRS"
+SCAN_LEN_EST = 10000#(s) guessing len of SOS, if wrong frame broken/merged
 
 def getFrame3(buffer, skip=False):
     '''
@@ -36,9 +45,10 @@ def getFrame3(buffer, skip=False):
                 print("potential header:", hex(buffer[i+1]))
     return len(buffer)
 
-# Start a socket listening for connections on 0.0.0.0:8000 (all addrs)
+# Start a socket listening for connections on 0.0.0.0:PORT_NUMBER (all addrs)
 server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8000))
+server_socket.bind(('0.0.0.0', PORT_NUMBER))
+server_socket.settimeout(SOCKET_TIMEOUT)
 server_socket.listen(0)
 
 
@@ -48,31 +58,27 @@ connection, addr = server_socket.accept()
 print("connection recvd", addr)
 
 #face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades
-
+backSub = cv2.createBackgroundSubtractorMOG2(100,16,False)
 try:
     buffer = bytearray()
     READ_LENGTH = 4096
     COUNTER = 0
-    if BACKGROUND_SUBTRACTOR:
-        backSub = cv2.createBackgroundSubtractorMOG2(
-            100,16,False
-            )
 
     while True:
         frame = bytearray()       
-        while True: #populates 1 frame, and leaves buffer with next frame start in it
-            buffer += connection.recv(READ_LENGTH)# FF D8 .. .. FF D9
-            i = getFrame3(buffer, COUNTER > 100)#points to last scanned byte in buffer
-            while i > len(buffer):#if skipped ahead read to the skipped index
+        while True: # populates 1 frame, and leaves buffer with next frame start in it
+            buffer += connection.recv(READ_LENGTH)# FF D8 .. .. FF D9 (MPJEG FRAME)
+            i = getFrame3(buffer, COUNTER > 10)# points to last scanned byte in buffer
+            while i > len(buffer):# if skipped ahead read to the skipped index
                 buffer += connection.recv(READ_LENGTH)
 
-            frame += buffer[:i+1]#add to frame everything up to including last scanned byte
-            buffer = buffer[i+1:]
+            frame += buffer[:i+1]# add to frame everything up to including last scanned byte
+            buffer = buffer[i+1:]# remove frame from buffer but keep data that exceeded the frame
             if len(frame) == 0 or frame[-2] == 0xFF and frame[-1] == 0xD9:
                 break
         if len(frame) == 0:
             break
-        #print(len(frame))
+
         numpy_frame = np.asarray(frame, dtype="uint8")
         image = cv2.imdecode(numpy_frame,cv2.IMREAD_COLOR)        
         
@@ -87,21 +93,11 @@ try:
         if CORNER_DETECTION:
             gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             corners = cv2.goodFeaturesToTrack(gray_scale,64,0.01,10)
-            #lineS,lineE=None,None
             try:
                 for i, corner in enumerate(corners):
                     x = int(corner[0][0])
                     y = int(corner[0][1])
-                    '''
-                    if i == 0:
-                        lineS = x,y
-                    else:
-                        lineE = x,y
-                        cv2.line(image, lineS,lineE,(0,255,0))
-                        lineS = x,y
-                    '''
                     cv2.circle(image, (x,y), 1, (0,255,0))
-                    #print(x,y)
             except:
                 pass
             
@@ -111,34 +107,32 @@ try:
         if COUNTOURS:
             threshold = 55
             gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray_scale = cv2.GaussianBlur(gray_scale, (5, 5), 0)# Gaussian blur to reduce noise
             canny_output = cv2.Canny(gray_scale, threshold, threshold * 2)
             contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            contours_poly = [None]*len(contours)
-            boundRect = [None]*len(contours)
-            centers = [None]*len(contours)
-            radius = [None]*len(contours)
-            for i, c in enumerate(contours):
-                contours_poly[i] = cv2.approxPolyDP(c, 3, True)
-                boundRect[i] = cv2.boundingRect(contours_poly[i])
-                centers[i], radius[i] = cv2.minEnclosingCircle(contours_poly[i])
-            drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-            if True:
-                # Draw polygonal contour + bonding rects + circles
-                for i in range(len(contours)):
-                    color = (0, 255, 0)
-                    cv2.drawContours(drawing, contours_poly, i, color)
-                    #cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), \
-                      #(int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
-                    #cv2.circle(drawing, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color, 2)
-            image = drawing
+            #drawing = np.zeros_like(image)# alternatively create a blank image and draw the contours on that
+            cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
+            #image = drawing
                 
         #image = cv2.resize(image,(1920,1080))
-        cv2.putText(image, f"{COUNTER}", (10,20),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,0))                 
+        cv2.putText(image, f"{COUNTER} b:{BACKGROUND_SUBTRACTOR} c:{CORNER_DETECTION} t:{THRESHOLDING} s:{COUNTOURS}", (10,20),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,0))                 
         cv2.imshow(WINDOW_NAME, image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
             break
+        elif key == ord('b'):
+            BACKGROUND_SUBTRACTOR = not(BACKGROUND_SUBTRACTOR)
+        elif key == ord('c'):
+            CORNER_DETECTION = not(CORNER_DETECTION)
+        elif key == ord('t'):
+            THRESHOLDING = not(THRESHOLDING)
+        elif key == ord('s'):
+            COUNTOURS = not(COUNTOURS)
         COUNTER += 1
+        
 finally:
+    print("connection closed", addr)
     connection.close()
     server_socket.close()
     cv2.destroyAllWindows()
